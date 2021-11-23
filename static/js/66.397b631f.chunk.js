e89160b9d704f1c93c762a42cced8a5e5cfc17a7
test <test@PrincedeMac-mini.local>
2021-11-23 17:48:34 +0800

    Updates

---
+++ static/js/66.397b631f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@uniswap/interface"]=this["webpackJsonp@uniswap/interface"]||[]).push([[66],{756:function(e,i,s){"use strict";s.r(i),i.default=s.p+"static/media/ru-RU.fd2ee0cb.po"}}]);
+//# sourceMappingURL=66.397b631f.chunk.js.map
